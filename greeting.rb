2035fcc8d5faee9f3e0d60b144b6a05f24c4cff4
Cameron Chafkar <ccdecisions@gmail.com>
2019-01-15 17:46:14 -0800

    correct greeting solution

--- greeting.rb
+++ greeting.rb
@@ -1,11 +1,5 @@
-def greeting
-  if ARGV[0] = "Hey"
-  ARGV.each do |arg|
-  puts "Hey, #{arg}"
-else
-  ARGV.each do |arg|
-    puts "Yo, #{{arg}}"
-end
-end
+greeting = ARGV.shift
 
-greeting
+ARGV.each do |name|
+  puts "#{greeting} #{name}"
+end
