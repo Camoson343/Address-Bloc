a2297ef7b0d82fdf64efaf91db5b694b10dc0016
Cameron Chafkar <ccdecisions@gmail.com>
2019-01-20 12:17:51 -0800

    first attempt

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,16 +9,24 @@ require_relative 'entry'
 
 
    def add_entry(name, phone_number, email)
-     # #9
      index = 0
      entries.each do |entry|
-     # #10
        if name < entry.name
          break
        end
        index+= 1
      end
-     # #11
      entries.insert(index, Entry.new(name, phone_number, email))
+   end
+
+   def remove_entry(name, phone_number, email)
+     index = 0
+     entries.each do |entry|
+       if name < entry.name
+         break
+       end
+       index+= 1
+     end
+     entries.delete(name, phone_number, email)
    end
  end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -35,5 +35,14 @@ require_relative '../models/address_book'
          expect(new_entry.phone_number).to eq('010.012.1815')
          expect(new_entry.email).to eq('augusta.king@lovelace.com')
        end
+
+       describe "#remove_entry" do
+         it "removes only one entry from the address book" do
+           book = AddressBook.new
+           entry = book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+           book.remove_entry(entry)
+           expect(book.entries.size).to eq(0)
+        end
+       end
      end
    end
