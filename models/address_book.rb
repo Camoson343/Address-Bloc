3cc214e6ebf8ce7e541d927b907048c5e8478119
Cameron Chafkar <ccdecisions@gmail.com>
2019-01-27 12:38:58 -0800

    address-bloc searching work

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,6 +20,26 @@ require "csv"
      entries.insert(index, Entry.new(name, phone_number, email))
    end
 
+   def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid -1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     return nil
+   end
+
    def import_from_csv(file_name)
      # Implementation goes here
      csv_text = File.read(file_name)
